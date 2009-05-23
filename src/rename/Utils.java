69567aefc0a0085481c952490498700e94b6bf2c
james <james@be3fd238-c909-4725-b312-79b88f520c4f>
2009-05-23 21:23:56 +0000

    About to test resize
    git-svn-id: http://192.168.1.66/svn/Java/trunk/rename@319 be3fd238-c909-4725-b312-79b88f520c4f

--- src/rename/Utils.java
+++ src/rename/Utils.java
@@ -1,6 +1,9 @@
 package rename;
 
+import com.jamescookie.graphics.ImageManipulator;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -55,9 +58,30 @@ public class Utils {
     public static void doResize(String directoryPath, int size) {
         File directory = new File(directoryPath);
         List<File> filesList = findFiles(directory);
+        Double scale;
 
         for (File file : filesList) {
+            String fileName = file.getName();
+            file.renameTo(new File(directory, SAFE_PREFIX + fileName));
+            File newfile = new File(directory, fileName);
+            try {
+                FileOutputStream out = new FileOutputStream(newfile);
+                ImageManipulator im = new ImageManipulator(file, out);
 
+                if (im.getWidth() > im.getHeight()) {
+                    scale = (double) im.getWidth() * ((double) size / 100 );
+                } else {
+                    scale = (double) im.getHeight() * ((double) size / 100 );
+                }
+
+                im.createThumbnail(scale.intValue());
+                out.flush();
+                out.close();
+//                file.delete();
+//                newfile.renameTo(new File(directory, fileName));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
